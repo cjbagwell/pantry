29032b07a5f0ec26fb3fd383678036dee765d54e
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-21 11:23:14 +0000

    Added in removeIngredient, viewPantry, and refillPantry blank activities

--- app/src/main/java/com/example/pantry/MainActivity.java
+++ app/src/main/java/com/example/pantry/MainActivity.java
@@ -9,22 +9,63 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 private Button addIngredientButton;
+private Button removeIngredientButton;
+private Button viewPantryButton;
+private Button refillPantryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addIngredientButton = (Button) findViewById(R.id.addIngredientButton);
+        addIngredientButton     = (Button) findViewById(R.id.addIngredientButton);
+        removeIngredientButton  = (Button) findViewById(R.id.removeIngredientButton);
+        viewPantryButton        = (Button) findViewById(R.id.viewPantryButton);
+        refillPantryButton      = (Button) findViewById(R.id.refillPantryButton);
         addIngredientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openAddIngredientActivity();
             }
         });
+
+        removeIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRemoveIngredientActivity();
+            }
+        });
+
+        viewPantryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openViewPantryActivity();
+            }
+        });
+
+        refillPantryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRefillPantryActivity();
+            }
+        });
+
+
     }
     public void openAddIngredientActivity(){
         Intent intent = new Intent(this, AddIngredientActivity.class);
+        startActivity(intent);
+    }
+    public void openRemoveIngredientActivity(){
+        Intent intent = new Intent(this, RemoveIngredientActivity.class);
+        startActivity(intent);
+    }
+    public void openViewPantryActivity(){
+        Intent intent = new Intent(this, ViewPantryActivity.class);
+        startActivity(intent);
+    }
+    public void openRefillPantryActivity(){
+        Intent intent = new Intent(this, RefillPantryActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/pantry/RemoveIngredientActivity.java
@@ -0,0 +1,14 @@
+package com.example.pantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemoveIngredientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_ingredient);
+    }
+}
