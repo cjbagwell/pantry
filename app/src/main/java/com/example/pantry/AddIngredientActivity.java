8c20d10f43bab18e0353a033d1ee68aae634ab11
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-21 10:51:18 +0000

    working button to addIngredientActivity

---
+++ app/src/main/java/com/example/pantry/AddIngredientActivity.java
@@ -0,0 +1,14 @@
+package com.example.pantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddIngredientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ingredient);
+    }
+}
--- app/src/main/java/com/example/pantry/MainActivity.java
+++ app/src/main/java/com/example/pantry/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.pantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+private Button addIngredientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addIngredientButton = (Button) findViewById(R.id.addIngredientButton);
+        addIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddIngredientActivity();
+            }
+        });
+    }
+    public void openAddIngredientActivity(){
+        Intent intent = new Intent(this, AddIngredientActivity.class);
+        startActivity(intent);
     }
 }
