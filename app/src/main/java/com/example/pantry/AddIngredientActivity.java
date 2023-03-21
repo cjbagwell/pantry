0d2b1c58184eff485ce8ac543e1c440076434647
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-21 22:44:15 +0000

    Chamged addIngredientActivity to have a view that will eventually accept the camera

--- app/src/main/java/com/example/pantry/AddIngredientActivity.java
+++ app/src/main/java/com/example/pantry/AddIngredientActivity.java
@@ -2,9 +2,28 @@ package com.example.pantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.util.ArrayList;
 
 public class AddIngredientActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView textView;
+    TextView pantryItemPreview;
+    ImageView imageView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/pantry/Ingredient.java
+++ app/src/main/java/com/example/pantry/Ingredient.java
@@ -1,2 +1,18 @@
-package com.example.pantry;public class Ingredient {
+package com.example.pantry;
+
+import android.graphics.Bitmap;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class Ingredient {
+    Barcode barcode;
+    String name;
+    Bitmap image;
+    int qtInPantry;
+
+    Ingredient(Barcode bar, String nam, Bitmap im, int qtInPan){
+        this.barcode = bar;
+        this.name = nam;
+        this.image = im;
+        this.qtInPantry = qtInPan;
+    }
 }
