402fbdb2a8a8bd62ded1645df11e89e9a18d38cd
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-22 10:36:03 +0000

    addIngredientActivity camera working

--- app/src/main/java/com/example/pantry/AddIngredientActivity.java
+++ app/src/main/java/com/example/pantry/AddIngredientActivity.java
@@ -1,9 +1,13 @@
 package com.example.pantry;
-
+import android.Manifest;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +17,7 @@ import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AddIngredientActivity extends AppCompatActivity {
@@ -22,6 +27,7 @@ public class AddIngredientActivity extends AppCompatActivity {
     TextView textView;
     TextView pantryItemPreview;
     ImageView imageView;
+    BarcodeDetector barcodeDetector;
 
 
 
@@ -29,5 +35,43 @@ public class AddIngredientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredient);
+
+        // Find views
+        surfaceView = findViewById(R.id.cameraPreview);
+        textView = findViewById(R.id.textView);
+        pantryItemPreview = findViewById(R.id.pantryItemPreview);
+        imageView = findViewById(R.id.imageView);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS).build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(720, 480)
+                .setAutoFocusEnabled(true) // TODO maybe make this a variable instead of just true
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback(){
+            @Override
+            public void surfaceCreated(SurfaceHolder holder){
+                if(ActivityCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    textView.setText("Permission to use camera not granted");
+                    return;
+                }
+                try{
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {cameraSource.stop();}
+        });
     }
 }
