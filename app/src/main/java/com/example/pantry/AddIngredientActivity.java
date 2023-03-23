25ae132f5f700d577adddab8aa935bc4dcf3777c
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-23 22:25:40 +0000

    Barcode Scanner is working

--- app/src/main/java/com/example/pantry/AddIngredientActivity.java
+++ app/src/main/java/com/example/pantry/AddIngredientActivity.java
@@ -2,11 +2,13 @@ package com.example.pantry;
 import android.Manifest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Button;
@@ -14,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
@@ -36,7 +39,7 @@ public class AddIngredientActivity extends AppCompatActivity {
     ImageView imageView;
     BarcodeScanner barcodeScanner;
     InputImage inputImage;
-
+    PreviewView previewView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,57 +47,82 @@ public class AddIngredientActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_ingredient);
 
         // Find views
-        surfaceView = findViewById(R.id.cameraPreview);
-        textView = findViewById(R.id.textView);
-        pantryItemPreview = findViewById(R.id.pantryItemPreview);
-        imageView = findViewById(R.id.imageView);
-
+//        surfaceView = findViewById(R.id.cameraPreview);
+//        textView = findViewById(R.id.textView);
+//        pantryItemPreview = findViewById(R.id.pantryItemPreview);
+//        imageView = findViewById(R.id.imageView);
+        previewView = findViewById(R.id.preview_view);
         BarcodeScannerOptions options = new BarcodeScannerOptions
                 .Builder()
                 .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
                 .build();
 //        inputImage = null;
 
-        barcodeScanner = BarcodeScanning.getClient();
+        barcodeScanner = BarcodeScanning.getClient(options);
 
         cameraSourceConfig = new CameraSourceConfig.Builder(this, barcodeScanner, new DetectionTaskCallback<List<Barcode>>() {
             @Override
             public void onDetectionTaskReceived(@NonNull Task<List<Barcode>> task) {
-                List<Barcode> barcodes = task.getResult();
-                if(barcodes.isEmpty()){
-                    return;
-                }
-                for(int i=0; i< barcodes.size(); i++){
-                    Barcode curBarcode = barcodes.get(i);
-                    Toast.makeText(AddIngredientActivity.this, curBarcode.getRawValue(), Toast.LENGTH_SHORT).show();
-                }
+                task = barcodeScanner.process(InputImage.fromBitmap(previewView.getBitmap(), 0));
+                task.addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                    @Override
+                    public void onSuccess(List<Barcode> barcodes) {
+                        if (barcodes.isEmpty()){
+                            return;
+                        }
+                        for (int i = 0; i < barcodes.size(); i++){
+                            Toast.makeText(AddIngredientActivity.this, barcodes.get(i).getDisplayValue(), Toast.LENGTH_SHORT).show();
+                        }
+//                        barcodeScanner.close(); //TODO: make the API Call
+                    }
+                });
+//
+//                List<Barcode> barcodes = task.getResult();
+//                if(barcodes.isEmpty()){
+//                    return;
+//                }
+//                for(int i=0; i< barcodes.size(); i++){
+//                    Barcode curBarcode = barcodes.get(i);
+//                    Toast.makeText(AddIngredientActivity.this, curBarcode.getRawValue(), Toast.LENGTH_SHORT).show();
+//                }
             }
         })
                 .setRequestedPreviewSize(720, 480)
+                .setFacing(CameraSourceConfig.CAMERA_FACING_BACK)
                 .build();
-        CameraXSource cameraXSource = new CameraXSource(cameraSourceConfig);
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback(){
-            @Override
-            public void surfaceCreated(SurfaceHolder holder){
-                if(ActivityCompat.checkSelfPermission(getApplicationContext(),
-                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                    textView.setText("Permission to use camera not granted");
-                    return;
-                }
-                try{
-                    cameraXSource.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {cameraXSource.stop();}
-        });
+        CameraXSource cameraXSource = new CameraXSource(cameraSourceConfig, previewView);
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            textView.setText("Permission to use camera not granted");
+            return;
+        }
+        try{
+            cameraXSource.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback(){
+//            @Override
+//            public void surfaceCreated(SurfaceHolder holder){
+//                if(ActivityCompat.checkSelfPermission(getApplicationContext(),
+//                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+//                    textView.setText("Permission to use camera not granted");
+//                    return;
+//                }
+//                try{
+//                    cameraXSource.start();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+//
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {cameraXSource.stop();}
+//        });
     }
 }
