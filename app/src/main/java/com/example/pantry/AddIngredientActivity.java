127bfcb2253cd2d13082e06e23d9d439986613a9
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-24 09:38:58 +0000

    Removed commented out code

--- app/src/main/java/com/example/pantry/AddIngredientActivity.java
+++ app/src/main/java/com/example/pantry/AddIngredientActivity.java
@@ -6,13 +6,7 @@ import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,19 +20,13 @@ import com.google.mlkit.vision.camera.CameraSourceConfig;
 import com.google.mlkit.vision.camera.CameraXSource;
 import com.google.mlkit.vision.camera.DetectionTaskCallback;
 import com.google.mlkit.vision.common.InputImage;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class AddIngredientActivity extends AppCompatActivity {
 
-    SurfaceView surfaceView;
     CameraSourceConfig cameraSourceConfig;
     TextView textView;
-    TextView pantryItemPreview;
-    ImageView imageView;
     BarcodeScanner barcodeScanner;
-    InputImage inputImage;
     PreviewView previewView;
 
     @Override
@@ -47,16 +35,11 @@ public class AddIngredientActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_ingredient);
 
         // Find views
-//        surfaceView = findViewById(R.id.cameraPreview);
-//        textView = findViewById(R.id.textView);
-//        pantryItemPreview = findViewById(R.id.pantryItemPreview);
-//        imageView = findViewById(R.id.imageView);
         previewView = findViewById(R.id.preview_view);
         BarcodeScannerOptions options = new BarcodeScannerOptions
                 .Builder()
                 .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
                 .build();
-//        inputImage = null;
 
         barcodeScanner = BarcodeScanning.getClient(options);
 
@@ -76,17 +59,8 @@ public class AddIngredientActivity extends AppCompatActivity {
 //                        barcodeScanner.close(); //TODO: make the API Call
                     }
                 });
-//
-//                List<Barcode> barcodes = task.getResult();
-//                if(barcodes.isEmpty()){
-//                    return;
-//                }
-//                for(int i=0; i< barcodes.size(); i++){
-//                    Barcode curBarcode = barcodes.get(i);
-//                    Toast.makeText(AddIngredientActivity.this, curBarcode.getRawValue(), Toast.LENGTH_SHORT).show();
-//                }
-            }
-        })
+            }//onTaskDetectionReceived
+        })//cameraSourceConfig.Builder
                 .setRequestedPreviewSize(720, 480)
                 .setFacing(CameraSourceConfig.CAMERA_FACING_BACK)
                 .build();
@@ -101,28 +75,5 @@ public class AddIngredientActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback(){
-//            @Override
-//            public void surfaceCreated(SurfaceHolder holder){
-//                if(ActivityCompat.checkSelfPermission(getApplicationContext(),
-//                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-//                    textView.setText("Permission to use camera not granted");
-//                    return;
-//                }
-//                try{
-//                    cameraXSource.start();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-//
-//            }
-//
-//            @Override
-//            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {cameraXSource.stop();}
-//        });
-    }
+    }//onCreate
 }
