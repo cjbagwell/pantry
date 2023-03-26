2ad45f57e4091297f506b3344c82c2dfb402e2df
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-26 08:52:29 +0000

    Changed Ingredient Class to have getters setters and toString

--- app/src/main/java/com/example/pantry/Ingredient.java
+++ app/src/main/java/com/example/pantry/Ingredient.java
@@ -4,15 +4,60 @@ import android.graphics.Bitmap;
 import com.google.android.gms.vision.barcode.Barcode;
 
 public class Ingredient {
-    Barcode barcode;
-    String name;
-    Bitmap image;
-    int qtInPantry;
+    private Barcode barcode;
+    private String name;
+    private String image;
+    private int qtInPantry;
 
-    Ingredient(Barcode bar, String nam, Bitmap im, int qtInPan){
+    Ingredient(Barcode bar, String nam, String im, int qtInPan){
         this.barcode = bar;
         this.name = nam;
         this.image = im;
         this.qtInPantry = qtInPan;
+    }
+
+    // To String Method
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "name='" + name + '\'' +
+                ", qtInPantry=" + qtInPantry +
+                '}';
+    }
+
+
+    // ********************* GETTERS AND SETTERS *****************************
+
+    public Barcode getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(Barcode barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getQtInPantry() {
+        return qtInPantry;
+    }
+
+    public void setQtInPantry(int qtInPantry) {
+        this.qtInPantry = qtInPantry;
     }
 }
