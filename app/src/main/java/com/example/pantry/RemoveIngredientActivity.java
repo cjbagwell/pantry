1613916acb4b8924bb1c86f196107035ca15694c
Jordan B <cjbagwell@crimson.ua.edu>
2023-03-29 18:55:32 +0100

    RemoveIngredientActivity is working

--- app/src/main/java/com/example/pantry/RemoveIngredientActivity.java
+++ app/src/main/java/com/example/pantry/RemoveIngredientActivity.java
@@ -1,14 +1,159 @@
 package com.example.pantry;
 
+import android.Manifest;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RemoveIngredientActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.camera.CameraSourceConfig;
+import com.google.mlkit.vision.camera.CameraXSource;
+import com.google.mlkit.vision.camera.DetectionTaskCallback;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+
+public class RemoveIngredientActivity extends AppCompatActivity implements RemoveIngredientPopUpDialog.RemoveIngredientPopUpDialogListener {
+
+    CameraSourceConfig cameraSourceConfig;
+    TextView textView;
+    BarcodeScanner barcodeScanner;
+    BarcodeScannerOptions barcodeScannerOptions;
+    PreviewView previewView;
+    PantryManager manager;
+    CameraXSource cameraXSource;
+    ImageView imageViewCheckMark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_ingredient);
+        // Create variables
+        manager = new PantryManager(this);
+        // Find views
+        previewView = findViewById(R.id.preview_view);
+        imageViewCheckMark = findViewById(R.id.imageViewCheckMark);
+        barcodeScannerOptions = new BarcodeScannerOptions
+                .Builder()
+                .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+                .build();
+
+        barcodeScanner = BarcodeScanning.getClient(barcodeScannerOptions);
+
+        cameraSourceConfig = new CameraSourceConfig.Builder(this, barcodeScanner, new DetectionTaskCallback<List<Barcode>>() {
+            @Override
+            public void onDetectionTaskReceived(@NonNull Task<List<Barcode>> task) {
+                task = barcodeScanner.process(InputImage.fromBitmap(previewView.getBitmap(), 0));
+                task.addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                    @Override
+                    public void onSuccess(List<Barcode> barcodes) {
+                        if (barcodes.isEmpty()) {
+                            return;
+                        }
+                        barcodeScanner.close();
+                        cameraXSource.stop();
+                        imageViewCheckMark.setVisibility(ImageView.VISIBLE);
+                        // Process the result with a pop up dialog
+                        setResult(manager.getIngredient(barcodes.get(0).getRawValue()));
+                    }
+                });
+            }//onTaskDetectionReceived
+        })//cameraSourceConfig.Builder
+                .setRequestedPreviewSize(720, 480)
+                .setFacing(CameraSourceConfig.CAMERA_FACING_BACK)
+                .build();
+        cameraXSource = new CameraXSource(cameraSourceConfig, previewView);
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            textView.setText("Permission to use camera not granted");
+            return;
+        }
+        try {
+            cameraXSource.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//onCreate
+
+    public void setResult(Ingredient result) {
+        // If the ingredient is not in the database, restart the camera and present a message
+        if (result == null) {
+            Toast.makeText(this, "Ingredient does not exist in the online database :(", Toast.LENGTH_LONG).show();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            imageViewCheckMark.setVisibility(ImageView.GONE);
+            barcodeScanner = BarcodeScanning.getClient(barcodeScannerOptions);
+            cameraXSource.start();
+            return;
+        }
+        Toast.makeText(this, "We got a result", Toast.LENGTH_LONG).show();
+        openPopUpDialog(result);
     }
+
+    public void openPopUpDialog(Ingredient ingredient) {
+        Bundle bundle = new Bundle();
+        bundle.putString("barcode", ingredient.getBarcode());
+
+        RemoveIngredientPopUpDialog popUpDialog = new RemoveIngredientPopUpDialog();
+        popUpDialog.setArguments(bundle);
+        popUpDialog.show(getSupportFragmentManager(), "pop up dialog");
+    }
+
+    @Override
+    public void getPopUpResult(String barcode, int mQuantityToChange) {
+        manager.addToPantry(barcode, -mQuantityToChange);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        imageViewCheckMark.setVisibility(ImageView.GONE);
+        barcodeScanner = BarcodeScanning.getClient(barcodeScannerOptions);
+        cameraXSource.start();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+    @Override
+    protected void onDestroy() {
+        cameraXSource.close();
+        cameraXSource.stop();
+        barcodeScanner.close();
+
+        super.onDestroy();
+    }
+    @Override
+    public void onBackPressed(){
+        barcodeScanner.close();
+        super.onBackPressed();
+    }
+
 }
